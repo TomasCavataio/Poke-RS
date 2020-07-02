4dc73af56ab995caf85ce7798bbf74160e0ad09d
tomascavataio <tomascavataio@gmail.com>
2020-07-02 20:47:47 +0200

    pokemon Generator!

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const pokemonBase = "https://pokeapi.co/api/v2/pokemon";
+const pokemonImage = document.querySelector(".pokemon-image");
+const name = document.querySelector(".name");
+const pokeball = document.querySelector(".pokeball");
+const openPokeball = document.querySelector(".open-pokeball");
+const types = document.querySelector(".types");
+
+/**
+ *  Fetches a random pokemon through pókeAPI.
+ */
+async function getData() {
+  const randomPokemon = Math.floor(Math.random() * 150);
+
+  setTimeout(function () {
+    (pokemonImage.style.display = "block"),
+      (openPokeball.style.display = "none");
+  }, 500);
+
+  return await fetch(`${pokemonBase}/${randomPokemon}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      pokemonImage.src = data.sprites.front_default;
+      name.innerText = data.name;
+      for (let t of data.types) {
+        types.innerText += " | " + t.type.name;
+      }
+      pokemonImage.style.display = "none";
+      openPokeball.style.display = "block";
+    })
+    .catch((error) => {
+      console.error("Cannot get data", error);
+    });
+}
+
+getData();
+
+document.querySelector(".pokeball").addEventListener("click", getData);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./serviceWorker.js").then(function () {
+    console.log("Service Worker Registered");
+  });
+}
---
+++ serviceWorker.js
@@ -0,0 +1,28 @@
+self.addEventListener('fetch', function (e) {
+    e.waitUntil(
+        caches.open('poke-cache')
+            .then((cache) => {
+                return cache.addAll(
+                    ['./',
+                        './index.html',
+                        './assets/js/app.js',
+                        './assets/css/style.css',
+                        './assets/css/reset.css']
+                )
+            }));
+});
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request)
+            .then(async response => {
+                if (event.request.method === 'GET') {
+                    caches.open('poke-cache').then(cache => cache.add(event.request)).catch(console.warn);
+                }
+                if (!response) throw response
+                return response;
+            })
+            .catch(() => caches.match(event.request)),
+    );
+});
