cad49a495d94965b9d4acd02339912bc7527ebfb
tomascavataio <tomascavataio@gmail.com>
2020-07-03 15:37:19 +0200

    Improve serviceWorker, add favicon

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,22 +1,21 @@
-self.addEventListener('fetch', function (e) {
-    e.waitUntil(
-        caches.open('poke-cache')
-            .then((cache) => {
-                return cache.addAll(
-                    ['./',
-                        './index.html',
-                        './assets/js/app.js',
-                        './assets/css/style.css',
-                        './assets/css/reset.css']
-                )
-            }));
-});
+
+caches.open('poke-cache')
+    .then((cache) => {
+        return cache.addAll(
+            ['./',
+                './index.html',
+                './assets/js/app.js',
+                './assets/css/style.css',
+                './assets/css/reset.css']
+        )
+    });
 
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         fetch(event.request)
             .then(async response => {
+                console.log(event.request);
                 if (event.request.method === 'GET') {
                     caches.open('poke-cache').then(cache => cache.add(event.request)).catch(console.warn);
                 }
